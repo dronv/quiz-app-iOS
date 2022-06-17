82db10c676c404e4267b650f1bda27c280d6cdea
dronv <dronvyas4454@gmail.com>
2022-06-17 01:04:03 -0400

    Initial Commit

---
+++ quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by Dron Vyas on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
