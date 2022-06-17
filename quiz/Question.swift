16000d75bec4def1c80510d0f2f2697c467a79da
dronv <dronvyas4454@gmail.com>
2022-06-17 01:16:01 -0400

    first commit

---
+++ quiz/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  quiz
+//
+//  Created by Dron Vyas on 6/17/22.
+//
+
+import Foundation
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
